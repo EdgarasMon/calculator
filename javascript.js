74b7505007f861cf8aa1189831b1bcc015f18713
EdgarasMon <edgarasxxx@gmail.com>
2021-10-11 20:46:07 +0300

    ,

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
 let result = document.querySelector('#result');
 let number_buttons = document.querySelectorAll('.number-button');
-let value = document.querySelector(".number-button");
 let current_val = "";
-let endNumber = "";
+let full_number = "";
 
  function CreateCurrentInput(){
     let input = event.target.value;
     current_val += input; 
     console.log("current_val ",current_val);
-    if (event.target.value != "+"){
-        return endNumber = current_val += input; 
-    }
+    
 }
 
  function Add(){
