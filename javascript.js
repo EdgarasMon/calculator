f19938a27b2eab154a2b370909339033d40a9d54
EdgarasMon <edgarasxxx@gmail.com>
2021-10-13 22:29:02 +0300

    .

--- javascript.js
+++ javascript.js
@@ -5,33 +5,32 @@ let current_number = 0;
 let isNumberFirst = true;
 let operator = '';
 
-// concatinates currently inputed numbers
  function CreateCurrentInput() {
     let input = event.target.value;
-    current_number == 0 ? current_number += parseInt(input) : current_number += input;
-    console.log("current_number ", parseInt(current_number)); // paparsinau i int
+    // concatinates currently inputed numbers
+    current_number == 0 ? current_number += parseInt(input) : current_number += input; 
+    console.log("current_number ", current_number);
 }
 
 function AddToPreviousNumber() {
-    previous_number += current_number;  // blogai sujungia kai pridedam trecia skaiciu
-    console.log(typeof(previous_number))
-    console.log(typeof(current_number))
-    isNumberFirst = false;
+    previous_number += parseInt(current_number);
+    isNumberFirst = false; // CHECK IF ITS NEEDED
     console.log("previous_number inside AddToPreviousNumber() ", previous_number);
 }
 
 function DeductFromPreviousNumber() {
+    console.log("previous_number D() ", previous_number);
     if (isNumberFirst) {
         previous_number = current_number;
     } else {
         previous_number -= current_number;
     }
     isNumberFirst = false; 
-    console.log("previous_number inside DeductFromPreviousNumber() ", previous_number);
+    console.log("previous_number D() ", previous_number);
 }
 
 function ResetCurrentInput() {
-    current_number = '';
+    current_number = 0;
 }
 
 function Add() {
@@ -49,13 +48,13 @@ function Add() {
  }
 
 function CountResult() {
-    var res = 0;
-    if (operator == 'toAdd') {
-       res = parseInt(previous_number) + parseInt(current_number);
+    if (operator === 'toAdd') {
+        AddToPreviousNumber();
+        ResetCurrentInput();
     }
-    else if (operator == 'toDeduct') {
-        res = parseInt(previous_number) - parseInt(current_number);
+    else if (operator === 'toDeduct') {
+        DeductFromPreviousNumber();
+        ResetCurrentInput();
     }
-
-    console.log("result = ", res);
+    console.log("result = ", previous_number);
 }
