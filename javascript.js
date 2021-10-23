554272ec5ce46af7ccef41a820643f7ecd4cdb5e
EdgarasMon <edgarasxxx@gmail.com>
2021-10-23 19:40:43 +0300

    Fixed bug calculator

--- javascript.js
+++ javascript.js
@@ -15,55 +15,40 @@ let playSound3 = () => new Audio("https://www.soundjay.com/buttons/sounds/button
     let input = event.target.value;
     // concatinates currently inputed numbers
     current_number == 0 ? current_number += parseInt(input) : current_number += input;    
-    console.log("previous_number ", previous_number); 
-    console.log("current_number ", current_number);
     current_number_el.innerText = current_number;
-    memoField.innerText += current_number;
+    memoField.innerText += " " + current_number;
 }
 
 function AddToPreviousNumber() {
-    console.log("current_number inside AddToPreviousNumber() ", current_number);  
-    console.log("previous_number inside AddToPreviousNumber() ", previous_number);  
     previous_number += parseInt(current_number);
     isNumberFirst = false;
-    console.log("previous_number inside AddToPreviousNumber() ", previous_number);
-    //memoField.innerText += ' + '; 
 }
 
 function DeductFromPreviousNumber() {
-    console.log("previous_number D() ", previous_number);
     if (isNumberFirst) {
         previous_number = current_number;
     } else {
         previous_number -= current_number;
     }
     isNumberFirst = false; 
-    console.log("previous_number D() ", previous_number);
-    //memoField.innerText += ' - ';
 }
 
 function MultiplyFromPreviousNumber() {
-    console.log("previous_number D() ", previous_number);
     if (isNumberFirst) {
         previous_number = current_number;
     } else {
         previous_number *= current_number;
     }
     isNumberFirst = false; 
-    console.log("previous_number D() ", previous_number);
-    //memoField.innerText += ' * ';
 }
 
 function DivideFromPreviousNumber() {
-    console.log("previous_number D() ", previous_number);
     if (isNumberFirst) {
         previous_number = current_number;
     } else {
         previous_number /= current_number;
     }
     isNumberFirst = false; 
-    console.log("previous_number D() ", previous_number);
-    //memoField.innerText += ' / ';
 }
 
 function ResetCurrentInput() {
@@ -74,12 +59,11 @@ function ResetPreviousInput() {
     previous_number = 0;
 }
 
-function CallAdditionOperand() {
-    if (isNumberFirst) operator='toAdd'; 
+function CallAdditionOperand() { 
+    if (isNumberFirst) operator='toAdd';
     DoPreviousOperation();
     ResetCurrentInput();
     operator='toAdd';
-    console.log("+")
     memoField.innerText += ' + ';    
  }
 
@@ -88,7 +72,6 @@ function CallAdditionOperand() {
     DoPreviousOperation();
     ResetCurrentInput();
     operator='toDeduct';
-    console.log("-")
     memoField.innerText += ' - ';
  }
 
@@ -97,7 +80,6 @@ function CallAdditionOperand() {
     DoPreviousOperation();
     ResetCurrentInput();
     operator='toMultiply';
-    console.log("*")
     memoField.innerText += ' * ';
  }
 
@@ -106,28 +88,23 @@ function CallAdditionOperand() {
     DoPreviousOperation();
     ResetCurrentInput();
     operator='toDivide';
-    console.log("/")
-    memoField.innerText += ' / ';
+    memoField.innerText += ' / ' ;
  }
 
 function DoPreviousOperation() {
     if (operator === 'toAdd') {
-        console.log("DoPreviousOperation - toAdd");
         AddToPreviousNumber();
         ResetCurrentInput();
     }
     else if (operator === 'toDeduct') {
-        console.log("DoPreviousOperation - toDeduct");
         DeductFromPreviousNumber();
         ResetCurrentInput();
     }
     else if (operator === 'toMultiply'){
-        console.log("DoPreviousOperation - toMultiply");
         MultiplyFromPreviousNumber();    
         ResetCurrentInput();
     }
     else if (operator === 'toDivide'){
-        console.log("DoPreviousOperation - toDivide");
         DivideFromPreviousNumber();    
         ResetCurrentInput();
     }
@@ -138,14 +115,10 @@ function CountResult() {
     console.log("result = ", previous_number);
     result_el.innerText = previous_number;
     memoField.innerText += ' = ';
-    memoField.innerText += previous_number;
+    memoField.innerText += " " + previous_number;
     memoField.innerText += "\n";
-    ResetCurrentInput(); //naujai
-    ResetPreviousInput(); // naujai
-
-    //let memField = [];
-    //memField.push(String(CreateCurrentInput()));
-    //console.log("labas", memField)
-    //memoField.innerText = memField; // memo laukui
-
+    operator = '';
+    ResetCurrentInput();
+    ResetPreviousInput();
+    isNumberFirst = true;
 }
